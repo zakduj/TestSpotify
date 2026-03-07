949cad6bc1ee510a570e35fc3bc0e90c0091018a
zakduj <zakoduj@gmail.com>
2026-03-06 19:26:38 -0500

    feat dependances

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -13,5 +13,12 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/api/**': { proxy: 'http://127.0.0.1:3000/**' }
+  },
+
+  runtimeConfig: {
+    apiBase: process.env.NUXT_API_BASE || 'http://localhost:3000',
+    public: {
+      apiBase: 'http://localhost:3000'
+    }
   }
 });
